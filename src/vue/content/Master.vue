0d9ca057b813be66350a5609627574cb6925f0dd
申鹏浩 <mrshenph@gmail.com>
2025-06-12 19:56:32 +0800

    修改版权所有中的网址

--- src/vue/content/Master.vue
+++ src/vue/content/Master.vue
@@ -48,7 +48,7 @@
     <FooterBlock :darken="true"
                  :row="false">
       <FooterCopyright holder="YiHao Fastener"
-                       url="www.yihaofastener.com"
+                       url=""
                        license="All Rights Reserved." />
     </FooterBlock>
   </Footer>
