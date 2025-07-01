27ffa36ab9f540b3499461dcef788d2c296adce0
申鹏浩 <mrshenph@gmail.com>
2025-07-01 17:41:24 +0800

    修改底部Copyright跳转

--- src/vue/components/footer/FooterCopyright.vue
+++ src/vue/components/footer/FooterCopyright.vue
@@ -1,36 +1,42 @@
 <template>
-    <p class="foxy-footer-copyright text-1"
-       v-html="formattedCopyright"/>
+  <p class="foxy-footer-copyright text-1"
+     v-html="formattedCopyright" />
 </template>
 
 <script setup>
-import {computed} from "vue"
-import {useStrings} from "/src/composables/strings.js"
+import { computed } from 'vue'
+import { useStrings } from '/src/composables/strings.js'
 
 const strings = useStrings()
 
 const props = defineProps({
-    holder: String,
-    url: String,
-    license: String,
+  holder: String,
+  url: String,
+  license: String,
 })
 
 const formattedCopyright = computed(() => {
-    return strings.getCopyrightMessage(
-        new Date().getFullYear().toString(),
-        props.holder,
-        props.url,
-        props.license
-    )
+  return strings.getCopyrightMessage(
+    new Date().getFullYear().toString(),
+    props.holder,
+    '',
+    props.license
+  )
+  // return strings.getCopyrightMessage(
+  //     new Date().getFullYear().toString(),
+  //     props.holder,
+  //     props.url,
+  //     props.license
+  // )
 })
 </script>
 
 <style lang="scss" scoped>
-@import "/src/scss/_theming.scss";
+@import '/src/scss/_theming.scss';
 
 p {
-    color: $light-5;
-    padding: 0;
-    margin: 0;
+  color: $light-5;
+  padding: 0;
+  margin: 0;
 }
 </style>
